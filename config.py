0554a5e26944eb857dfb1b438adc93531e0d1b86
IgnacioGrosman <121733674+IgnacioGrosman@users.noreply.github.com>
2024-11-06 18:03:38 -0300

    Agregado gunicorn a requirements.txt

--- config.py
+++ config.py
@@ -8,9 +8,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = (
+
+    # Usar DATABASE_URL si está definido (por ejemplo, en producción), si no, usar configuración local
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or (
         f"postgresql://{os.environ.get('DB_USERNAME')}:{os.environ.get('DB_PASSWORD')}"
         f"@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
     )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
